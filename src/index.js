0052b9a2b206c6a319afaa5f230ec86e21530876
Abanim2 <115773506+Abanim2@users.noreply.github.com>
2023-04-27 11:07:09 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
+// const fs = require("fs/promises");
+// const fileName = "myfile.txt";
+// const reader = async (fileName) => {
+//   // write code here
+// 	// dont change function name
+  
+// };
+
+
+// module.exports =  reader ;
+
+
+
+
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
+
 const reader = async (fileName) => {
-  // write code here
-	// dont change function name
-  
+  try {
+    // read the content of the file
+    const content = await fs.readFile(fileName, "utf-8");
+    return content;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-
-module.exports =  reader ;
+module.exports = reader;
